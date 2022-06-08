e8ea782b4d7032f950fb5a69df1d8eab5a1278f8
kendrik <kendrik_lim@artc.a-star.edu.sg>
2022-06-09 02:41:15 +0800

    Chuck Norris Joke Generator
    # Week 5
    Florin Pop

---
+++ jokes-generator/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const API_URL = `http://api.icndb.com/jokes/random`;
+
+function App() {
+  const [joke, setJoke] = useState('');
+
+  const generateJoke = () => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setJoke(data.value.joke));
+  };
+
+  useEffect(() => {
+    generateJoke();
+  }, []);
+
+  return (
+    <div className="box">
+      <h2>Chuck Norris Jokes Generator</h2>
+      <p dangerouslySetInnerHTML={{__html: joke}}/>
+      <button onClick={generateJoke}>Get new joke</button>
+    </div>
+  );
+}
+
+export default App;
